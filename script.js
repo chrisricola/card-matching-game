f51e525c4d8f88bac608ca735bf1a3cadad4af3d
Chris Rico <chris.r.rico@gmail.com>
2021-04-22 16:11:07 -0700

    added sound function for game start, over, victory

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
-}
+  class AudioController {
+      constructor() {
+          this.bgMusic = new Audio('Assets/Audio/creepy.mp3');
+          this.flipSound = new Audio('Assets/Audio/flip.wav');
+          this.matchSound = new Audio('Assets/Audio/match.wav');
+          this.victorySound =  new Audio('Assets/Audio/victory.wav');
+          this.gameOverSound =  new Audio('Assets/Audio/gameover.wav');
+          this.bgMusic.volume = 0.5;
+          this.bgMusic.loop = true;
+      }
+      startMusic() {
+          this.bgMusic.play();
+      }
+      stopMusic() {
+          this.bgMusic.pause();
+          this.bgMusic.currentTime = 0;
+      }
+      flip() {
+          this.flipSound.play();
+      }
+      match() {
+          this.matchSound.play();
+      }
+      victory() {
+          this.stopMusic();
+          this.victory.play();
+      }
+      gameOver() {
+          this.stopMusic();
+          this.gameOverSound.play();
+      }
+  }
 
 function ready() {
     let overlays =  Array.from(document.getElementsByClassName('overlay-text'));
@@ -11,6 +38,20 @@ function ready() {
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
+            // game.startGame();
+            let audioController = new AudioController();
+            audioController.startMusic();
         });
     });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            // game.flipCard(card);
+        });
+    });
+}
+
+if(document.readyState === 'loading') {
+    document.addEventListener9('DOMContentLoaded', ready());
+} else {
+    ready();
 }
