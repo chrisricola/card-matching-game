c41b2f63f239bc6d1eaaccd534a0563af1158661
Chris Rico <chris.r.rico@gmail.com>
2021-04-23 13:33:55 -0700

    the click sound and the number of flips function

--- script.js
+++ script.js
@@ -31,21 +31,51 @@
       }
   }
 
+  class MixOrMatch {
+      constructor(totalTime, cards) {
+          this.cardsArray = cards;
+          this.totalTime = totalTime;
+          this.timeRemaining = totalTime;
+          this.timer = document.getElementById('time-remaining');
+          this.ticker = document.getElementById('flips');
+          this.audioController = new AudioController();
+      }
+      startGame() {
+          this.cardToCheck = null;
+          this.totalClicks = 0;
+          this.timeRemaining = this.totalTime;
+          this.matchedCards = [];
+          this.busy = true;
+      }
+      flipCard(card) {
+        if (this.canFlipCard(card)) {
+            this.audioController.flip();
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+        }
+      }
+      canFlipCard(card) {
+          return true;
+        //   return(!this.busy && !this.matchedCards.includes(card) && card !==this.cardToCheck)
+      }
+  }
+
 function ready() {
     let overlays =  Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            // game.startGame();
-            let audioController = new AudioController();
-            audioController.startMusic();
+            game.startGame();
+            // let audioController = new AudioController();
+            // audioController.startMusic();
         });
     });
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            // game.flipCard(card);
+            game.flipCard(card);
         });
     });
 }
